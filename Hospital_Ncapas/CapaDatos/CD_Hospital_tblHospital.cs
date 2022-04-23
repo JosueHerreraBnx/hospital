fc1b8dce57b5918d78f0e19f13efc3d87e474f80
JosueHerreraBnx <102001504+JosueHerreraBnx@users.noreply.github.com>
2022-04-22 19:13:09 -0500

    Add files via upload

---
+++ Hospital_Ncapas/CapaDatos/CD_Hospital_tblHospital.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Hospital_tblHospital
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar() { 
+       
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrartblHospital";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+            
+        }
+
+        public void Insertar(string nombre, string apellido, DateTime ingreso,int altas,string consultas, string analisis, string procedimientos ) {
+            //PROCEDIMNIENTO
+            
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsetartblHosptal";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre",nombre);
+            comando.Parameters.AddWithValue("@apellido", apellido);
+            comando.Parameters.AddWithValue("@ingreso", ingreso);
+            comando.Parameters.AddWithValue("@altas", altas);
+            comando.Parameters.AddWithValue("@consultas", consultas);
+            comando.Parameters.AddWithValue("@analisis", analisis);
+            comando.Parameters.AddWithValue("@procedimientos", procedimientos);
+
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        
+        }
+
+        public void Editar(string nombre, string apellido, DateTime ingreso, int altas, string consultas, string analisis, string procedimientos,int id)
+        {
+            
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditartblHosptal";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@apellido", apellido);
+            comando.Parameters.AddWithValue("@ingreso", ingreso);
+            comando.Parameters.AddWithValue("@altas", altas);
+            comando.Parameters.AddWithValue("@consultas", consultas);
+            comando.Parameters.AddWithValue("@analisis", analisis);
+            comando.Parameters.AddWithValue("@procedimientos", procedimientos);
+            comando.Parameters.AddWithValue("@id",id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+
+        public void Eliminar(int id) {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminartblHosptal";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@idpro",id);
+
+            comando.ExecuteNonQuery();
+
+            comando.Parameters.Clear();
+        }
+        private bool Contraseña(string password)
+        {
+           bool mayuscula = false, minuscula = false, numero = false, carespecial = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (Char.IsUpper(password, i))
+                {
+                    mayuscula = true;
+                }
+                else if (Char.IsLower(password, i))
+                {
+                    minuscula = true;
+                }
+                else if (Char.IsDigit(password, i))
+                {
+                    numero = true;
+                }
+                else
+                {
+                    carespecial = true;
+                }
+            }
+            if (mayuscula && minuscula && numero && carespecial && password.Length >= 8)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Hospital_Ncapas/CapaNegocio/CN_Hospital.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class CN_Hospital
+    {
+        private CD_Hospital_tblHospital objetoCD = new CD_Hospital_tblHospital();
+
+        public DataTable MostrarProd() {
+
+            DataTable tabla = new DataTable();
+            tabla = objetoCD.Mostrar();
+            return tabla;
+        }
+        public void InsertarPRod(string nombre, string apellido, DateTime ingreso, int altas, string consultas, string analisis, string procedimientos)
+        {
+
+            objetoCD.Insertar(nombre, apellido, ingreso, altas, consultas,analisis,procedimientos); 
+    }
+
+        public void EditarProd(string nombre, string apellido, string ingreso, int altas, string consultas, string analisis, string procedimientos,string id)
+        {
+            objetoCD.Editar(nombre, apellido, Convert.ToDateTime(ingreso), altas, consultas, analisis, procedimientos, Convert.ToInt32(id));
+        }
+
+        public void EliminarPRod(string id) {
+
+            objetoCD.Eliminar(Convert.ToInt32(id));
+        }
+
+    }
+}
